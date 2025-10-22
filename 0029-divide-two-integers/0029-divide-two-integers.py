fafaaa0f7d86e4c732a708d9809d643967a9d750
Its-soul <1948immortalsoul@gmail.com>
2025-10-22 21:31:13 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.76%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,5 +1,7 @@
-class Solution:
-    def divide(self, dividend: int, divisor: int) -> int:
+class Solution(object):
+    def divide(self, dividend, divisor):
         if dividend == -2**31 and divisor == -1:
             return 2**31 - 1
-        return int(dividend / divisor)
+        return int(float(dividend)/ divisor)
+# in older version of python we donot get the float value so we must use float on the dividend
+# to get that point value..
