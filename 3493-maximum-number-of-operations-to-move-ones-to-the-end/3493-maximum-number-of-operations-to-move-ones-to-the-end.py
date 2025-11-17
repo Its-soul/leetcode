36af883111e639574e953d4e4e324d5839a7a61a
Its-soul <1948immortalsoul@gmail.com>
2025-11-17 22:12:20 +0530

    Time: 70 ms (70.69%), Space: 12.9 MB (96.61%) - LeetHub

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/3493-maximum-number-of-operations-to-move-ones-to-the-end.py
@@ -2,11 +2,14 @@ class Solution(object):
     def maxOperations(self, s):
         ones = 0
         count = 0
-        n = len(s)
-        for i in range(n):
-            if s[i] == '1':
+        prev = None
+        
+        for char in s:
+            if char == '1':
                 ones += 1
             else:
-                if i > 0 and s[i-1] == '1':
+                if prev == '1':
                     count += ones
+            prev = char
+        
         return count
