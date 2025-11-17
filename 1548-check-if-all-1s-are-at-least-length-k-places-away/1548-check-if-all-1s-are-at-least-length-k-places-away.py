a4d33d44a8bbaa07a922c3b24cf3145248fe9115
Its-soul <1948immortalsoul@gmail.com>
2025-11-17 19:17:43 +0530

    Time: 15 ms (46.15%), Space: 16.3 MB (48.08%) - LeetHub

---
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def kLengthApart(self, nums, k):
+        count = k 
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                if count < k:
+                    return False
+                count = 0
+            else:
+                count += 1
+        return True
