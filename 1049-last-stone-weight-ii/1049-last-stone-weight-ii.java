681620f15459f937108328bc6416bb0a221b8c39
Its-soul <1948immortalsoul@gmail.com>
2025-12-09 23:12:32 +0530

    Time: 5 ms (35.11%), Space: 42.9 MB (45.66%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -1,42 +1,39 @@
 class Solution {
     public int lastStoneWeightII(int[] stones) {
-        int n = stones.length;
-        int sum = 0;
-        for(int i=0; i<n; i++) {
-            sum+=stones[i];
+        int n= stones.length;
+        int total=0;
+        for (int i: stones){
+            total+=i;
         }
-        int target = sum/2;
-        boolean[][] dp = new boolean[n][target+1];
+        int sum= total/2;
+        boolean [][] dp = new boolean [n][sum+1];
 
-        for(int i=0; i<=target; i++) {
+        for(int i=0; i<=sum; i++) {
             if(stones[0]==i) {
                 dp[0][i]=true;
             }
         }
-        for(int i=1; i<n; i++) {
-            dp[i][0] = true;
+
+        for (int i=0; i<dp.length;i++){
+            dp[i][0]= true;
         }
-        for(int i=1; i<dp.length; i++) {
-            for(int j=1; j<dp[0].length; j++) {
-                int curr = stones[i];
-                if(curr == j) {
-                    dp[i][j] = true;
-                }else if(j>curr) {
-                    dp[i][j] = dp[i-1][j-curr]||dp[i-1][j];
-                }else {
-                    dp[i][j] = dp[i-1][j];
+        for(int i=1; i<dp.length;i++){
+            for (int j=1; j<dp[0].length;j++){
+                int curr=stones[i];
+                if (j==curr) dp[i][j]=true;
+                else if(j>curr){
+                    dp[i][j]= dp[i-1][j-curr] || dp[i-1][j];
                 }
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        int sum1 = 0;
-        for(int i=target; i>=0; i--) {
-            if(dp[n-1][i]==true) {
-                sum1 = i;
+        int ans=0;
+        for (int i=sum;i>=0;i--){
+            if (dp[n-1][i]== true){
+                ans=i;
                 break;
             }
         }
-        int sum2 = sum - sum1;
-
-        return Math.abs(sum1-sum2);
+        return total-2*ans;  
     }
 }
