7ba26eaf9a40c9f2706da14c45ec873d536e9137
Its-soul <1948immortalsoul@gmail.com>
2025-12-09 22:24:09 +0530

    Time: 18 ms (25.83%), Space: 54.9 MB (40.96%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,22 +1,34 @@
 class Solution {
-    public int change(int amount, int[] coins) {
-        int n = coins.length;
-        Integer[][] dp = new Integer[n][amount + 1];
-        return helper(n - 1, coins, amount, dp);
+    public int change( int amount,int[] coins) {
+        Integer[][] dp=new Integer[coins.length][amount+1];
+        return helper(coins,amount,coins.length-1,dp); 
     }
-
-    public int helper(int i, int[] coins, int amount, Integer[][] dp) {
-        if (amount == 0) return 1;
-        if (i < 0) return 0;
-        if (dp[i][amount] != null) return dp[i][amount];
-        if (i==0){
-            if(amount%coins[0]==0) return 1;
-            else return 0;
+    public int helper(int[] coins,int target,int i,Integer[][] dp){
+        if(target==0){
+            return 1;
         }
-        int notTake = helper(i - 1, coins, amount, dp);
-        int take = 0;
-        if (coins[i] <= amount) take = helper(i, coins, amount - coins[i], dp);
-
-        return dp[i][amount] = notTake+take;
+        if(i==0){
+            if(target%coins[0]==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        if(target<0){
+            return 0;
+        }
+        if(dp[i][target]!=null){
+            return dp[i][target];
+        }
+        int take=0;
+        if(coins[i]<=target){
+            take=helper(coins,target-coins[i],i,dp);
+           
+        }
+        int nottake= helper(coins,target,i-1,dp);
+        
+        dp[i][target]=take+nottake;
+        return dp[i][target];
     }
 }
