ef9b8d3de97c4ced58aa0dbd636eeb1078872f4e
Its-soul <1948immortalsoul@gmail.com>
2025-12-20 01:05:09 +0530

    Time: 10 ms (93.93%), Space: 180.8 MB (10.09%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.java
+++ 0403-frog-jump/0403-frog-jump.java
@@ -8,26 +8,20 @@ class Solution {
 
     public boolean helper(int[] stones, int index, int lastJump, Boolean[][] dp) {
         if (index == stones.length - 1) return true;
-
         if (dp[index][lastJump] != null)
             return dp[index][lastJump];
-
         for (int jump = lastJump - 1; jump <= lastJump + 1; jump++) {
             if (jump <= 0) continue;
-
             int nextPos = stones[index] + jump;
-
             for (int i = index + 1; i < stones.length; i++) {
                 if (stones[i] == nextPos) {
                     if (helper(stones, i, jump, dp)) {
                         dp[index][lastJump] = true;
                         return true;
                     }
-                }
-                if (stones[i] > nextPos) break;
+                }if (stones[i] > nextPos) break;
             }
         }
-
         dp[index][lastJump] = false;
         return false;
     }
