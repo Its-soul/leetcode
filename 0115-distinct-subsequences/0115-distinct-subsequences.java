0eef5717cb6f2c90ba5ace35b2b86a7b9fc91f33
Its-soul <1948immortalsoul@gmail.com>
2025-12-20 11:12:52 +0530

    Time: 27 ms (7.94%), Space: 58 MB (8.55%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,23 @@
+class Solution {
+    Integer[][] dp;
+
+    public int numDistinct(String s, String t) {
+        dp = new Integer[s.length()][t.length()];
+        return helper(s, t, 0, 0);
+    }
+
+    private int helper(String s, String t, int i, int j) {
+        if (j == t.length()) return 1;
+        if (i == s.length()) return 0;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        int count = helper(s, t, i + 1, j);
+
+        if (s.charAt(i) == t.charAt(j)) {
+            count += helper(s, t, i + 1, j + 1); 
+        }
+
+        return dp[i][j] = count;
+    }
+}
