109224bd1e5a3503cbbb9467751710a97a247972
Its-soul <1948immortalsoul@gmail.com>
2025-12-23 11:57:09 +0530

    Time: 10 ms (45.03%), Space: 44.4 MB (72.85%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.java
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.java
@@ -4,19 +4,21 @@ class Solution {
             Arrays.sort(arr[i]);
         }
         Arrays.sort(arr, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            if (a[1] != b[1]) return a[1] - b[1];
-            return a[2] - b[2];
+            return b[0]*b[1]*b[2]- a[0]*a[1]*a[2];
         });
 
         int n = arr.length;
         int[] dp = new int[n];
-        int maxx = 0;
-        for (int curr = 0; curr < n; curr++) {
-            dp[curr] = arr[curr][2]; 
+        for (int i = 0; i < n; i++){
+            dp[i] = arr[i][2];
+        }
+        int maxx = dp[0];
+        for (int curr = 1; curr < n; curr++) {
             for (int prev = 0; prev < curr; prev++) {
-                if (arr[curr][0] >= arr[prev][0] && arr[curr][1] >= arr[prev][1] && arr[curr][2] >= arr[prev][2]) {
-                    dp[curr] = Math.max(dp[curr], dp[prev] + arr[curr][2]);
+                if (arr[curr][0] <= arr[prev][0] && arr[curr][1] <= arr[prev][1] && arr[curr][2] <= arr[prev][2]) {
+                    if (dp[curr]<dp[prev]+arr[curr][2]){
+                        dp[curr]=dp[prev]+arr[curr][2];
+                    }
                 }
             }
             maxx = Math.max(maxx, dp[curr]);
