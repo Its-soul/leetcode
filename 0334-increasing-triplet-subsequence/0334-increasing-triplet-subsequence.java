3fd10f9398a13f81f32342c63e863e00ae4c18ab
Its-soul <1948immortalsoul@gmail.com>
2025-12-24 11:36:42 +0530

    Time: 3 ms (18.61%), Space: 122.6 MB (41.63%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,19 +1,41 @@
+// class Solution {
+//     public boolean increasingTriplet(int[] nums) {
+//         ArrayList<Integer> temp = new ArrayList<>();
+//         temp.add(nums[0]);
+        
+//         for (int i = 1; i < nums.length; i++) {
+//             if (nums[i] > temp.get(temp.size() - 1)) {
+//                 temp.add(nums[i]);
+//             } else {
+//                 int index = Collections.binarySearch(temp, nums[i]);
+//                 if (index < 0) {
+//                     index = -(index + 1);
+//                 }
+//                 temp.set(index, nums[i]);
+//             }
+//         }
+//         return (temp.size()<3)? false:true;
+//     }
+// }
+
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        temp.add(nums[0]);
-        
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] > temp.get(temp.size() - 1)) {
-                temp.add(nums[i]);
-            } else {
-                int index = Collections.binarySearch(temp, nums[i]);
-                if (index < 0) {
-                    index = -(index + 1);
-                }
-                temp.set(index, nums[i]);
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+        int i = 0;
+        while (i < nums.length) {
+            int num = nums[i];
+
+            if (num < first) {
+                first = num;
+            } else if (num > first && num < second) {
+                second = num;
+            } else if (num > second) {
+                return true;
             }
+
+            i++;
         }
-        return (temp.size()<3)? false:true;
+        return false;
     }
 }
