ff87951b6d53c5f5e4b3ef9cf9411097bfb693bf
Its-soul <1948immortalsoul@gmail.com>
2025-12-24 11:21:22 +0530

    Time: 31 ms (7.4%), Space: 122.7 MB (41.63%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(nums[0]);
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > temp.get(temp.size() - 1)) {
+                temp.add(nums[i]);
+            } else {
+                int index = Collections.binarySearch(temp, nums[i]);
+                if (index < 0) {
+                    index = -(index + 1);
+                }
+                temp.set(index, nums[i]);
+            }
+        }
+        return (temp.size()<3)? false:true;
+    }
+}
