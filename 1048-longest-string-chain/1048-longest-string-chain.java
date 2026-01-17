f92ce22b8f01293f6c73cf79f7c292efca2e3bfb
Its-soul <1948immortalsoul@gmail.com>
2026-01-18 01:34:23 +0530

    Time: 51 ms (47.52%), Space: 46.6 MB (54.03%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -2,31 +2,28 @@ class Solution {
     public int longestStrChain(String[] words) {
         Arrays.sort(words, (a, b) -> a.length() - b.length());
         int n=words.length;
-        int[] dp=new int[n];
+        int [] dp = new int [n];
         Arrays.fill(dp,1);
         int res=1;
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<i;j++){
-                if(helper(words[i],words[j]) && dp[j]+1 > dp[i]){
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<i; j++){
+                if(helper(words[i], words[j]) && dp[j]+1 > dp[i]){
                     dp[i]=1+dp[j];
                 }
-            }
-            res=Math.max(res,dp[i]);
-        }
-        return res;
+            }res=Math.max(res, dp[i]);
+        }return res;
     }
     public boolean helper(String s1, String s2){
         if(s1.length() != s2.length()+1) return false;
-        int f = 0;
-        int s = 0;
-        while(f < s1.length()){
-            if (s < s2.length() && s1.charAt(f) == s2.charAt(s)){
-                f++;
-                s++;
+        int i = 0;
+        int j = 0;
+        while(i < s1.length()){
+            if (j < s2.length() && s1.charAt(i) == s2.charAt(j)){
+                i++;
+                j++;
             }else{
-                f++;
+                i++;
             }
-        }
-        return (f == s1.length() && s == s2.length()) ? true : false;
+        }return (i == s1.length() && j == s2.length()) ? true : false;
     }
 }
