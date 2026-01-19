c205a8fda02ccd51b7f96efaf3520fdff8ef8924
Its-soul <1948immortalsoul@gmail.com>
2026-01-20 00:16:29 +0530

    Time: 23 ms (50.52%), Space: 12.8 MB (50.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+from collections import Counter 
+class Solution(object):
+    def isAnagram(self, s, t):
+        if Counter(s)==Counter(t):
+            return True
+        else:
+            return False
+            
