5924984679de1fe18d3a96b8c683cfdca8fd6a0e
Its-soul <1948immortalsoul@gmail.com>
2026-02-19 23:11:03 +0530

    Time: 0 ms (100%), Space: 43.1 MB (17.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-        int [] dp = new int [nums.length];
+        int [] dp= new int[nums.length+1];
         Arrays.fill(dp,-1);
-        return helper(0,nums, dp);        
+        return helper(nums, 0, dp);
     }
-    public static int helper(int i, int[] nums,int[] dp){
+    public static int helper(int[] nums, int i, int [] dp){
         if(i>=nums.length) return 0;
-        if (dp[i]!=-1) return dp[i];
-        int include = nums[i]+ helper(i+2,nums, dp);
-        int exclude = helper(i+1, nums, dp);
-
-        return dp[i]=Math.max(include, exclude);
+        if(dp[i]!=-1) return dp[i];
+        int take= nums[i]+ helper(nums, i+2, dp);
+        int notake= helper(nums, i+1, dp);
+        return dp[i]=Math.max(take,notake);
     }
 }
