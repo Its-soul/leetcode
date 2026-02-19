293bdd4925843c0b5e4fed6b700d90b90046de75
Its-soul <1948immortalsoul@gmail.com>
2026-02-19 23:24:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.58%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
+        int n= nums.length;
         if (n == 1) return nums[0];
-        
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int case1 = helper(0, nums, dp1, n - 1);
-
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-        int case2 = helper(1, nums, dp2, n);
-        return Math.max(case1, case2);
+        int [] dp1= new int [n];
+        Arrays.fill(dp1,-1);
+        int [] dp2= new int [n];
+        Arrays.fill(dp2,-1);
+        return Math.max(helper(nums, 0, n-1, dp1), helper(nums, 1, n, dp2));
     }
-    public int helper(int i, int[] nums, int[] dp, int stop) {
-        if (i >= stop) return 0;
-        if (dp[i] != -1) return dp[i];
-
-        int include = nums[i] + helper(i + 2, nums, dp, stop);
-        int exclude = helper(i + 1, nums, dp, stop);
-
-        return dp[i] = Math.max(include, exclude);
+    public static int helper(int [] nums, int i, int n, int [] dp){
+        if(i>=n){
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int take= nums[i]+helper(nums, i+2, n, dp);
+        int notake= helper(nums, i+1, n, dp);
+        return dp[i]=Math.max(take,notake);
     }
 }
