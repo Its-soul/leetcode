162b7ebfea66b6f7eb941030aa108f1a766b5fb0
Its-soul <1948immortalsoul@gmail.com>
2026-02-20 12:30:43 +0530

    Time: 27 ms (34.8%), Space: 46.6 MB (6.94%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,42 +1,24 @@
 class Solution {
-    static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-      try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-        fw.write("0");
-      } catch (Exception _) {
-      }
-    }));
-   }
     public int minCost(int n, int[] cuts) {
+        int [] arr= new int [cuts.length+2];
         Arrays.sort(cuts);
-        int s=cuts.length;
-        int [] arr= new int [s+2];
-        arr[0]=0;
-        arr[s+1]= n;
-        for(int i =1; i<=s;i++){
-            arr[i]=cuts[i-1];
+        arr[0]= 0;
+        for(int i=1; i<=cuts.length; i++){
+            arr[i]= cuts[i-1];
         }
-        int [][] dp = new int [s+2][s+2];
-        for(int i=s; i>=1;i--){
-            for(int j=1; j<=s;j++){
-                if (i>j) continue;
-                int min = Integer.MAX_VALUE;
-                for(int idx=i;idx<=j; idx++){
-                    int cost= arr[j+1]-arr[i-1] +dp[i][idx-1]+dp[idx+1][j];
-                    min= Math.min(min, cost);
-                }dp[i][j]= min;  
-            }
-        // return helper(1,s,arr, dp);
-        }return dp[1][s];
+        arr[cuts.length+1]=n;
+        Integer [][] dp = new Integer [cuts.length+2][cuts.length+2];
+        return helper(arr, 1, cuts.length, dp);
+        
+    }
+    public int helper(int [] arr, int i, int j, Integer [][] dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        int min=(int)1e9;
+        for(int idx=i; idx<=j; idx++){
+            int cost= arr[j+1] - arr[i-1]+ helper(arr, i, idx-1, dp)+ helper(arr, idx+1, j, dp);
+            min= Math.min(min, cost);
+        }
+        return dp[i][j]= min;
     }
 }
-// public int helper(int i, int j, int [] arr, Integer [][] dp){
-//     int min= (int)1e9;
-//     if (i>j) return 0;
-//     if(dp[i][j]!=null) return dp[i][j];
-//     for(int idx=i;idx<=j; idx++){
-//         int cost= arr[j+1]-arr[i-1] +helper(i, idx-1, arr,dp)+helper(idx+1,j,arr,dp);
-//         min= Math.min(min, cost);
-//     }
-//     return dp[i][j]= min;
-// }
