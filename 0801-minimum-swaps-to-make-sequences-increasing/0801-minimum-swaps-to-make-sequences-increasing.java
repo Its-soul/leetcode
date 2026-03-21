5c5057d35dd494a6fb39c939f3e118d69434e19d
Its-soul <1948immortalsoul@gmail.com>
2026-03-22 01:02:02 +0530

    Time: 40 ms (7.47%), Space: 137.1 MB (5.4%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.java
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.java
@@ -1,27 +1,45 @@
 class Solution {
+    Integer[][] dp;
+
     public int minSwap(int[] nums1, int[] nums2) {
         int n = nums1.length;
+        dp = new Integer[n][2];
+        return Math.min(
+            helper(nums1, nums2, 0, 0),
+            helper(nums1, nums2, 0, 1)
+        );
+    }
 
-        int keep = 0; 
-        int swap = 1; 
+    private int helper(int[] nums1, int[] nums2, int i, int swapped) {
+        if (i == nums1.length) return 0;
 
-        for (int i = 1; i < n; i++) {
-            int newKeep = Integer.MAX_VALUE;
-            int newSwap = Integer.MAX_VALUE;
+        if (dp[i][swapped] != null) return dp[i][swapped];
 
-            if (nums1[i] > nums1[i - 1] && nums2[i] > nums2[i - 1]) {
-                newKeep = keep;
-                newSwap = swap + 1;
+        int prev1, prev2;
+
+        if (i == 0) {
+            prev1 = -1;
+            prev2 = -1;
+        } else {
+            prev1 = nums1[i - 1];
+            prev2 = nums2[i - 1];
+
+            if (swapped == 1) {
+                int temp = prev1;
+                prev1 = prev2;
+                prev2 = temp;
             }
-            if (nums1[i] > nums2[i - 1] && nums2[i] > nums1[i - 1]) {
-                newKeep = Math.min(newKeep, swap);
-                newSwap = Math.min(newSwap, keep + 1);
-            }
-
-            keep = newKeep;
-            swap = newSwap;
         }
 
-        return Math.min(keep, swap);
+        int ans = Integer.MAX_VALUE / 2;
+
+        if (nums1[i] > prev1 && nums2[i] > prev2) {
+            ans = helper(nums1, nums2, i + 1, 0);
+        }
+        if (nums1[i] > prev2 && nums2[i] > prev1) {
+            ans = Math.min(ans, 1 + helper(nums1, nums2, i + 1, 1));
+        }
+
+        return dp[i][swapped] = ans;
     }
 }
