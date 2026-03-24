8c6d10d41259c4cf8b64027873e130858e230cfb
Its-soul <1948immortalsoul@gmail.com>
2026-03-24 11:09:53 +0530

    Time: 32 ms (40.53%), Space: 153.4 MB (28.69%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -0,0 +1,39 @@
+// class Solution {
+//     public int[] maxSlidingWindow(int[] nums, int k) {
+//         int  [] ans= new int [nums.length-k+1];
+//         for(int i=0; i<=nums.length-k; i++){
+//             int maxx=Integer.MIN_VALUE;
+//             for(int j=i; j<i+k; j++){
+//                 if(nums[j]>maxx){
+//                     maxx=nums[j];
+//                 }
+//             }
+//             ans[i]=maxx;
+//         }
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int [] ans= new int [nums.length-k+1];
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0; i<nums.length; i++){
+            //
+             while(!dq.isEmpty() && dq.peekFirst()==i-k){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+                dq.pollLast();
+            }
+            dq.addLast(i);
+           
+            if(i>=k-1){
+                ans[i-k+1]=nums[dq.peekFirst()];
+            }
+
+        }
+        return ans;
+    }
+}
