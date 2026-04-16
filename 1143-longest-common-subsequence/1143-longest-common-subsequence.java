a314fe5543c809f468f79e88eef386a1da0e48c8
Its-soul <1948immortalsoul@gmail.com>
2026-04-16 08:38:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,18 +1,13 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        int n = text1.length();
-        int m = text2.length();
-        int [][] dp = new int [n+1][m+1];
-        for(int i=1; i<=n;i++){
-            for(int j=1; j<=m;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    dp[i][j]= 1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+        int count=0;
+        for(int i=0; i<text1.length(); i++){
+            for(int j=0; j<text2.length(); j++){
+                if(text1.charAt(i)==text2.charAt(j)){
+                    count++;
                 }
             }
         }
-        return dp[n][m];
+        return count;
     }
 }
