3f00816fc13c8b04b1514a13a405383c6e8eb21f
Its-soul <1948immortalsoul@gmail.com>
2026-04-17 13:32:14 +0530

    Time: 2 ms (43.27%), Space: 47.3 MB (45.8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,34 +1,37 @@
 class Solution {
-    public int findCircleNum(int[][] iscon) {
-        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
-        int n = iscon.length;
-        for(int i=0; i<n; i++){
-            adj.add(new ArrayList<>());
+    int [] parent;
+    public int findCircleNum(int[][] isConnected) {
+        parent = new int [isConnected.length];
+        for(int i=0; i<parent.length; i++){
+            parent[i]=i;
         }
-        for(int i=0; i<n;i++){
-            for (int j = 0; j < n; j++) {
-                if (iscon[i][j] == 1 && i != j) {
-                    adj.get(i).add(j);
+        for(int i=0; i<parent.length; i++){
+            for(int j=0; j<parent.length; j++){
+                if(isConnected[i][j]==1){
+                    union(i,j);
                 }
             }
+
         }
         int count=0;
-        boolean [] vis= new boolean [n];
-        for(int i=0; i<adj.size();i++){
-            if(!vis[i]){
-                dfs(adj, vis, i);
+        for(int i=0; i<parent.length; i++){
+            if(parent[i]==i){
                 count++;
             }
         }
-            
         return count;
+
     }
-    public void dfs(ArrayList<ArrayList<Integer>> adj , boolean [] vis, int src){
-        vis[src] = true;
-        for (int neighbor : adj.get(src)) {
-            if (!vis[neighbor]) {
-                dfs(adj, vis, neighbor);
-            }
+    public int find(int x){
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    public void union(int x, int y){
+        int parx= find(x);
+        int pary= find(y);
+        if(parx!=pary){
+            parent[pary]=parx;
         }
     }
+
 }
