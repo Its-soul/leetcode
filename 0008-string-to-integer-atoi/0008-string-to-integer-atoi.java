217b6b42388a43672d95462381d6cfcfea5da649
Its-soul <1948immortalsoul@gmail.com>
2026-04-18 20:46:18 +0530

    Time: 5 ms (4.03%), Space: 44.5 MB (5.78%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -24,11 +24,11 @@ class Solution {
             i++;
         }
         if(num.equals(""))return 0;
-        if(num.length()>=11 && sign==1) return Integer.MAX_VALUE;
-        if(num.length()>=11 && sign==-1) return Integer.MIN_VALUE;
+        if(num.length()>=11 && sign==1)return Integer.MAX_VALUE;
+        if(num.length()>=11 && sign==-1)return Integer.MIN_VALUE;
         long value = Long.parseLong(num);
-        if (sign * value > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        if (sign * value < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+        if (sign* value> Integer.MAX_VALUE)return Integer.MAX_VALUE;
+        if (sign* value< Integer.MIN_VALUE)return Integer.MIN_VALUE;
 
         return (int)(sign * value);
     }
