7fe82df8860538ee7fbf618183969a8fd1e17a2a
Its-soul <1948immortalsoul@gmail.com>
2026-04-18 23:31:58 +0530

    Time: 39 ms (9.21%), Space: 48 MB (5.85%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,30 +1,23 @@
 class Solution {
+    Integer [][] dp;
     public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int [][]dp = new int [n+1][amount+1];
-        int inf= amount+1;
-        for(int i=0; i<=n;i++){
-            Arrays.fill(dp[i],inf);
-            dp[i][0]=0;
-        }
-        for(int i =1; i<=n;i++){
-            for(int j=1; j<=amount;j++){
-                dp[i][j]= dp[i-1][j];
-                if(coins[i-1] <= j){
-                    dp[i][j]= Math.min(dp[i][j], dp[i][j-coins[i-1]]+1);
-                }
+        dp = new Integer[coins.length][amount+1];
+        return helper(coins, amount , 0);
+    }
+    public int helper(int[] coins, int amount, int i){
+        if(amount==0) return 0;
+        if(i==coins.length || amount<0) return -1;
+        if(dp[i][amount]!=null) return dp[i][amount];
+        int take= -1;
+        if(coins[i]<=amount){
+            take= helper(coins, amount-coins[i], i);
+            if(take!=-1){
+                take++;
             }
         }
-        return dp[n][amount]>= amount+1 ? -1 : dp[n][amount];
+        int notake= helper(coins, amount, i+1);
+        if(take==-1) return dp[i][amount]=notake;
+        if(notake==-1) return dp[i][amount]=take;
+        return dp[i][amount]=Math.min(take, notake);
     }
-    // public int helper(int[] coins, int amount, int i){
-    //     if (amount<0 ||i==coins.length) return -1;
-    //     if (amount==0 ) return 0;
-    //     int take = helper(coins, amount-coins[i], i);
-    //     if (take!=-1) take++;
-    //     int notake = helper(coins, amount, i+1);
-    //     if (take == -1) return notake;
-    //     if (notake == -1) return take;
-    //     return Math.min(take, notake);
-    // }
 }
