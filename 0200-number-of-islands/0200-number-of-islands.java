8a84f039b77e805409e8705ccdb294e133a93f77
Its-soul <1948immortalsoul@gmail.com>
2026-04-18 21:46:21 +0530

    Time: 4 ms (43.23%), Space: 52.2 MB (70.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,27 +1,27 @@
 class Solution {
-    static int [] dr= {-1,1,0,0};//up, dowm, left, right
-    static int [] dc= {0,0,-1,1};
+    boolean[][]vis;
+    int count=0;
     public int numIslands(char[][] grid) {
-        int count=0;
-        boolean [][] vis= new boolean [grid.length][grid[0].length];
+        vis= new boolean[grid.length][grid[0].length];
         for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length;j++){
-                if(grid[i][j]=='1' && !vis[i][j]){
-                    dfs(grid, vis, i, j);
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]=='1'&& !vis[i][j]){
+                    dfs(grid, i, j);
                     count++;
                 }
             }
         }
         return count;
     }
-    public static void dfs(char[][] grid, boolean[][] vis, int i, int j){
-        vis[i][j]=true;
-        for(int k=0; k<4; k++){
-            int nR= i+dr[k];
-            int nC= j+dc[k];
-            if(nR>=0 && nC>=0 && nR<grid.length && nC<grid[0].length && !vis[nR][nC] && grid[nR][nC]=='1'){
-                dfs(grid, vis, nR, nC);
-            }
-        }
+
+    public void dfs(char[][] grid, int i, int j){
+        if (i<0 || j<0 || i>= grid.length || j>= grid[0].length) return;
+        if (grid[i][j]== '0' || vis[i][j]) return;
+        vis[i][j]= true;
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
