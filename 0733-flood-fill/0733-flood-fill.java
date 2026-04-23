2a4b7ee0b7595ecf2fe30cd84958108587424500
Its-soul <1948immortalsoul@gmail.com>
2026-04-23 23:47:01 +0530

    Time: 0 ms (100%), Space: 46.8 MB (63.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,25 +1,21 @@
 class Solution {
-    // up= -1,0, down=1,0, right= 0,1, left= 0,-1; 
-    static int [] dr= {-1,1,0,0};
-    static int [] dc= {0,0,1,-1};
+    int [] dr = {1,-1,0,0};
+    int [] dc = {0,0,-1,1};
+    int n;
+    int m;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int sc_color= image[sr][sc];
-        int rowl= image.length;
-        int coll= image[0].length;
-        boolean [][] vis = new boolean[rowl][coll];
-        dfs(sr, sc, image, color, vis, sc_color);
+        n = image.length;
+        m = image[0].length;
+        int original= image[sr][sc];
+        if(original==color) return image;
+        helper(image, sr,sc, color, original);
         return image;
-        
     }
-    public void dfs(int i, int j, int[][] image, int color, boolean[][] vis, int sc_color){
-        int rowl= image.length;
-        int coll= image[0].length;
-        if(i<0 || j<0 || i>=rowl|| j>=coll || image[i][j]!=sc_color) return;
-        if(vis[i][j]==true) return;
-        vis[i][j]=true;
+    public void helper(int[][]image, int i, int j, int color, int original){
+        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=original) return;
         image[i][j]=color;
-        for(int k= 0; k<4; k++){
-            dfs(i+dr[k], j+dc[k], image, color, vis, sc_color);
+        for(int k=0; k<4; k++){
+            helper(image, i+dr[k], j+dc[k], color, original);
         }
     }
 }
