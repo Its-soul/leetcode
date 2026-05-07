698331dedc179de248a7c3c75b608b2bd90fdab2
Its-soul <1948immortalsoul@gmail.com>
2026-05-07 12:16:00 +0530

    Time: 65 ms (5.5%), Space: 92 MB (36.15%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean canArrange(int[] arr, int k) {
         for(int i=0; i<arr.length; i++){
-            arr[i] = ((arr[i]%k)+k)%k;
+            arr[i]= ((arr[i]%k)+k)%k;
         }
-
+        int left=0;
+        int right= arr.length-1;
         Arrays.sort(arr);
-        int left = 0 , right =arr.length-1;
+        System.out.print(Arrays.toString(arr));
         while(left<=right){
-            if(arr[left]==0 ) {
-                left++; 
+            if(arr[left]==0){
+                left++;
                 continue;
             }
-           
-
-            if(arr[left]+arr[right]!=k || left==right) return false;
+            int sum=arr[left]+arr[right];
+            if(sum!=k || left==right){
+                return false;
+            }
             left++;
             right--;
         }
-
         return true;
     }
 }
