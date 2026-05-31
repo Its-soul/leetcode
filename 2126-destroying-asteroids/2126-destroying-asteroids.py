caa3bb9e9617b836523574c740df08b7c3ddc4e0
Its-soul <1948immortalsoul@gmail.com>
2026-05-31 21:38:42 +0530

    Time: 106 ms (48.28%), Space: 21.3 MB (72.41%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.py
+++ 2126-destroying-asteroids/2126-destroying-asteroids.py
@@ -1,11 +1,9 @@
 class Solution(object):
     def asteroidsDestroyed(self, mass, ast):
         ast.sort()
-        summ= mass
-        if mass<ast[0]:
-            return False
-        for i in range(len(ast)-1):
-            if summ+ast[i]<ast[i+1]:
+        for a in ast:
+            if mass < a:
                 return False
-            summ+=ast[i]
+            mass += a
+
         return True
