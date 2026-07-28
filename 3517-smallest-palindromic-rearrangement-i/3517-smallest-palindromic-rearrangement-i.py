b64ad5984d5edcbcde6fdacce4221fe89d1e57c5
Its-soul <1948immortalsoul@gmail.com>
2026-07-29 00:08:44 +0530

    Time: 317 ms (36.87%), Space: 21 MB (51.15%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def smallestPalindrome(self, s: str) -> str:
+        freq = [0] * 26
+        for c in s:
+            freq[ord(c) - ord('a')] += 1
+        left = []
+        middle = ''
+
+        for i in range(26):
+            left.append(chr(i + ord('a')) * (freq[i] // 2))
+
+            if freq[i] % 2 == 1:
+                middle = chr(i + ord('a'))
+
+        left = ''.join(left)
+
+        return left + middle + left[::-1]
