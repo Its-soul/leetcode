f7c1338b3901c8e50c8931c4cea116231570b0ce
Its-soul <1948immortalsoul@gmail.com>
2026-08-01 19:46:12 +0530

    Time: 0 ms (100%), Space: 42.8 MB (41.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,14 +1,30 @@
+// class Solution {
+//     public boolean predictTheWinner(int[] nums) {
+//         int res= helper(nums, 0, nums.length-1);
+//         if (res>=0) return true;
+//         return false;
+        
+//     }
+//     public int helper(int[] nums, int i, int j){
+//         if (i==j) return nums[i];
+//         int leftpick= nums[i]- helper(nums, i+1, j);
+//         int rightpick= nums[j]- helper(nums, i, j-1);
+//         return Math.max(leftpick,rightpick);
+//     }
+// }
+
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int res= helper(nums, 0, nums.length-1);
-        if (res>=0) return true;
-        return false;
+        int n = nums.length;
+        int[] dp = nums.clone(); 
         
-    }
-    public int helper(int[] nums, int i, int j){
-        if (i==j) return nums[i];
-        int leftpick= nums[i]- helper(nums, i+1, j);
-        int rightpick= nums[j]- helper(nums, i, j-1);
-        return Math.max(leftpick,rightpick);
+        for (int diff = 1; diff < n; ++diff) {
+            for (int j = n - 1; j - diff >= 0; --j) {
+                int i = j - diff;
+                dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+            }
+        }
+        
+        return dp[n - 1] >= 0;
     }
 }
