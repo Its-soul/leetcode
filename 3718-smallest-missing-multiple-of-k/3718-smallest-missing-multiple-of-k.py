238bb9381845c85b541e26aa0169fe91e7d80223
Its-soul <1948immortalsoul@gmail.com>
2026-08-25 19:02:15 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.54%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def missingMultiple(self, nums, k):
         i = 1
-        while True:
+        while i<= 101:
             if k * i not in nums:
                 return k * i
             i = i+1
