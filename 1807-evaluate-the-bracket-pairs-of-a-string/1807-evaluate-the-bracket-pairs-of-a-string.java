f7cb1e0df2d89c416ad4ed044ecf7cb728c2dc26
Its-soul <1948immortalsoul@gmail.com>
2026-09-26 19:48:58 +0530

    Time: 32 ms (90.95%), Space: 91 MB (65.16%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -1,3 +1,34 @@
+// class Solution {
+
+//     HashMap<String, String> map = new HashMap<>();
+
+//     public String evaluate(String s, List<List<String>> knowledge) {
+//         for (List<String> x : knowledge) {
+//             map.put(x.get(0), x.get(1));
+//         }
+
+//         return dfs(s);
+//     }
+
+//     public String dfs(String s) {
+//         int r = s.indexOf(')');
+
+//         if (r == -1) {
+//             return s;
+//         }
+
+//         int l = s.lastIndexOf('(', r);
+
+//         String left = s.substring(0, l);
+//         String key = s.substring(l + 1, r);
+//         String right = s.substring(r + 1);
+
+//         String value = map.getOrDefault(key, "?");
+
+//         return dfs(left + value + right);
+//     }
+// }
+
 class Solution {
 
     HashMap<String, String> map = new HashMap<>();
@@ -7,24 +38,20 @@ class Solution {
             map.put(x.get(0), x.get(1));
         }
 
-        return dfs(s);
-    }
+        StringBuilder ans = new StringBuilder();
 
-    public String dfs(String s) {
-        int r = s.indexOf(')');
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                int j = s.indexOf(')', i);
+                String key = s.substring(i + 1, j);
 
-        if (r == -1) {
-            return s;
+                ans.append(map.getOrDefault(key, "?"));
+                i = j;
+            } else {
+                ans.append(s.charAt(i));
+            }
         }
 
-        int l = s.lastIndexOf('(', r);
-
-        String left = s.substring(0, l);
-        String key = s.substring(l + 1, r);
-        String right = s.substring(r + 1);
-
-        String value = map.getOrDefault(key, "?");
-
-        return dfs(left + value + right);
+        return ans.toString();
     }
 }
